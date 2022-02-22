698d29f10c76e82023ee4e99ca1bbe46d912f9fc
Aruna Shamil <shamil.a@phystech.edu>
2022-02-22 19:39:40 +0300

    Printed stack without destroying it

---
+++ homework3/copy_stack.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+#include<stack>
+
+void printStack(std::stack<int> &s)
+{
+    vector<int> a;
+    std::stack<int> copy_s = s;
+
+    while (!copy_s.empty())
+    {
+        cout << copy_s.top() << endl;
+        int& wh = copy_s.top();
+        a.push_back(wh);
+        copy_s.pop();
+    }
+}
+
+int main()
+{
+    std::stack<int> s;
+    for (int i = 0; i < 10; i++)
+    {
+        s.push(i);
+    }
+
+    cout << "First call" << endl;
+    printStack(s);
+
+    cout << "Second call" << endl;
+    printStack(s);
+
+}
+
