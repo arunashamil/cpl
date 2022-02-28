08750c0eae0b088cac147324ac8299eaf00e7283
Aruna Shamil <shamil.a@phystech.edu>
2022-03-01 00:21:27 +0300

    Containers competition

---
+++ homework3/containers_1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    cout << "start a_capacity = " << a.capacity() << endl;
+    cout << "////////////////////////////////" << endl;
+
+    for (int i = 0; i < 100; i++)
+    {
+        a.push_back(i);
+        cout << "a[" << i << "] = " << a[i] << endl;
+        cout << "current a_capacity = " << a.capacity() << endl;
+    }
+
+    cout << "////////////////////////////////" << endl;
+
+    vector<int> b;
+    cout << "start b_capacity = " << b.capacity() << endl;
+    cout << "////////////////////////////////" << endl;
+    b.reserve(10);
+    for (int i = 0; i < 100; i++)
+    {
+        b.push_back(i);
+        cout << "b[" << i << "] = " << b[i] << endl;
+        cout << "current b_capacity = " << b.capacity() << endl;
+    }
+
+    cout << "////////////////////////////////" << endl;
+
+    vector<int> c;
+    c.reserve(10000000000000000);
+    cout << "start c_capacity = " << c.capacity() << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        b.push_back(i);
+        cout << "b[" << i << "] = " << b[i] << endl;
+        cout << "current b_capacity = " << b.capacity() << endl;
+    }
+
+    /* При запросе большой памяти возникает необработанное исключение, 
+    дальнейшая часть программы, в том числе и вывод значения переменной c_capacity, не выполняется*/
+
+	return 0;
+}
---
+++ homework3/containers_2.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <ctime>
+#include <array>
+#include <algorithm>
+#include <tuple>
+#include <list>
+#include <forward_list>
+
+const int t = 10000;
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	deque<int> d;
+	array<int, t> a;
+	static list<int> l;
+	forward_list<int> f_1;
+	forward_list<int> f_2;
+	for (int i = 0; i < t; i++)
+	{
+		int random = std::rand() % t;
+		v.push_back(random);
+		d.push_back(random);
+		a[i] = random;
+		l.push_back(random);
+		f_2.push_front(random);
+	}
+
+	f_2.reverse();
+
+	cout << "/////////// vector ///////////" << endl;
+
+	double start_time_v = clock(); // начальное время
+	sort(v.begin(), v.end());
+	double end_time_v = clock(); // конечное время
+	double search_time_v = end_time_v - start_time_v; // искомое время
+
+	cout << "search_time_v = " << search_time_v << endl;
+	cout << "////////////////////////////////" << endl;
+
+
+	cout << "/////////// deque ///////////" << endl;
+
+	double start_time_d = clock(); // начальное время
+	sort(d.begin(), d.end());
+	double end_time_d = clock(); // конечное время
+	double search_time_d = end_time_d - start_time_d; // искомое время
+
+	cout << "search_time_d = " << search_time_d << endl;
+
+	cout << "////////////////////////////////" << endl;
+
+
+	cout << "/////////// array ///////////" << endl;
+
+	double start_time_a = clock(); // начальное время
+	sort(a.begin(), a.end());
+
+	double end_time_a = clock(); // конечное время
+	double search_time_a = end_time_a - start_time_a; // искомое время
+
+	cout << "search_time_a = " << search_time_a << endl;
+
+	cout << "////////////////////////////////" << endl;
+
+	cout << "/////////// list ///////////" << endl;
+
+	double start_time_l = clock(); // начальное время
+	l.sort();
+	double end_time_l = clock(); // конечное время
+	double search_time_l = end_time_l - start_time_l; // искомое время
+
+	cout << "search_time_l = " << search_time_l << endl;
+
+
+	cout << "////////////////////////////////" << endl;
+
+
+
+	cout << "/////////// forward_list ///////////" << endl;
+
+	double start_time_f_2 = clock(); // начальное время
+	f_2.sort();
+	double end_time_f_2 = clock(); // конечное время
+	double search_time_f_2 = end_time_f_2 - start_time_f_2; // искомое время
+	cout << "search_time_f_own_sort = " << search_time_f_2 << endl;
+
+
+	cout << "////////////////////////////////" << endl;
+
+	vector<double> maximum_the_hormone = {search_time_a, search_time_d, search_time_f_2, search_time_l, search_time_v };
+
+	auto minmax = minmax_element(maximum_the_hormone.begin(), maximum_the_hormone.end());
+	cout << "AND THE WINNER IS " << *minmax.first << endl;
+	return 0;
+}
